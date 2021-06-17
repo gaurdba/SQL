3865d3fb11e894496e8fa3afda75ebb981385420
gaurdba <sanjaylko86@gmail.com>
2021-06-17 23:06:10 +0530

    Create check_listener.sh
    The script will Display the total number of connections for the given start & End timestamp, the third argument is optional for the overall connection.
    
    [oracle@mypc ~]$ ./check_listener.sh '17-JUN-2021 21:31' '2021-06-17T22:31' ora19pdb
    
    
    Processing Logfile 
    Logfile::/ora/admin/diag/tnslsnr/mypc/listener/trace/listener.log 
    
    
    Total Connection From Service ora19pdb== 347
    Total Connection From sqlplus Program== 347
    Total Connection From mypc.example.com Host== 347
    
    
    [oracle@mypc ~]$

---
+++ shell_script/check_listener.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#!/bin/ksh
+# Author	:Sanjay Gaur
+# Purpose	:Show the connection Details for given Start & End Timestamp
+#
+listener_dir=`lsnrctl status|grep 'Listener Log File'|awk '{print $4}'`
+list_file=`echo $listener_dir|awk -F "alert" '{print $1 "trace/listener.log" }'`
+if ! [ -f $list_file ]
+then
+	echo "Listener Logfile $list_file Doesn't Exists"
+	exit 0;
+fi
+start_time=$1
+end_time=$2
+srv_name=$3
+echo -e "\n"
+if [ $# -eq 0 ]
+then
+	echo "Enter the Start time, End Time  & Service_name"
+	exit 0;
+fi
+echo "Processing Logfile "
+echo -e "Logfile::$list_file \n\n"
+ini_line=`grep -no "$start_time" $list_file |head -1|awk -F ":" '{print $1}'`
+end_line=`grep -no "$end_time" $list_file|tail -1|awk -F ":" '{print $1}'`
+if ! [ $ini_line -gt 0 ]
+then
+   echo "No Line found for Entered Start Timestamp"
+	exit 0;
+fi
+if ! [ $end_line -gt 0 ]
+then
+   echo "No Line found for Entered Start Timestamp"
+        exit 0;
+fi
+
+#echo $ini_line $end_line
+sed -n "$ini_line","$end_line"p $list_file >/tmp/list.log
+if [ $# -lt 3 ]
+then
+	for srv in `cat /tmp/list.log|awk -F "SERVICE_NAME=" '{print $2}'|awk -F")" '{print $1}'|grep -v ^$|uniq`
+	do
+		echo "Total Connection From Service $srv==" `grep -c $srv /tmp/list.log`
+	done
+
+	for prg in `cat /tmp/list.log|awk -F "PROGRAM=" '{print $2}'|awk -F")" '{print $1}'|grep -v ^$|uniq`
+	do
+		echo "Total Connection From $prg Program==" `grep -c $srv /tmp/list.log`
+	done
+
+	for hst in `cat /tmp/list.log|awk -F "HOST=" '{print $2}'|awk -F")" '{print $1}'|grep -v ^$|uniq`
+	do
+		echo "Total Connection From $hst Host==" `grep -c $srv /tmp/list.log`
+	done
+exit;
+fi
+if [ $# -eq 3 ]
+then
+        for srv in `cat /tmp/list.log|grep $srv_name|awk -F "SERVICE_NAME=" '{print $2}'|awk -F")" '{print $1}'|grep -v ^$|uniq`
+        do
+                echo "Total Connection From Service $srv==" `grep -c $srv /tmp/list.log`
+        done
+
+        for prg in `cat /tmp/list.log|grep $srv_name|awk -F "PROGRAM=" '{print $2}'|awk -F")" '{print $1}'|grep -v ^$|uniq`
+        do
+                echo "Total Connection From $prg Program==" `grep -c $srv /tmp/list.log`
+        done
+
+        for hst in `cat /tmp/list.log|grep $srv_name|awk -F "HOST=" '{print $2}'|awk -F")" '{print $1}'|grep -v ^$|uniq`
+        do
+                echo "Total Connection From $hst Host==" `grep -c $srv /tmp/list.log`
+        done
+fi
+echo -e "\n"
